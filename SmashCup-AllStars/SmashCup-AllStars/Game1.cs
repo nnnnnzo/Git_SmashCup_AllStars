872c150e203d0e2948f0a94bc261b255a3fed6ce
nnnnnzo <enzo_lagadec@icloud.com>
2022-01-05 19:51:11 +0100

    Commentaires + jump J1 et J2

--- SmashCup-AllStars/SmashCup-AllStars/Game1.cs
+++ SmashCup-AllStars/SmashCup-AllStars/Game1.cs
@@ -25,6 +25,8 @@ namespace SmashCup_AllStars
         private int _vitessePerso2;
         private string animationP2;
         private string lastDirP2;
+        private bool jumpingP1, jumpingP2; //Is the character jumping?
+        private float startYP1, jumpspeedP1 = 0, startYP2, jumpspeedP2 = 0; //startY to tell us //where it lands, jumpspeed to see how fast it jumps
         /*blabla*/
         /*Test modif Gab*/
         public Game1()
@@ -36,16 +38,24 @@ namespace SmashCup_AllStars
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            //var joueur 1
             _perso1Position = new Vector2(200, 200);
             _vitessePerso1 = 200;
             animationP1 = "idleD";
             lastDirP1 = "D";
+            startYP1 = _perso1Position.Y;//Starting position
+            jumpingP1 = false;//Init jumping to false
+            jumpspeedP1 = 0;
 
+
+            //var joueur 2
             _perso2Position = new Vector2(600, 200);
             _vitessePerso2 = 200;
             animationP2 = "idleG";
             lastDirP2 = "G";
+            startYP2 = _perso2Position.Y;//Starting position
+            jumpingP2 = false;//Init jumping to false
+            jumpspeedP2 = 0;
 
             base.Initialize();
         }
@@ -59,73 +69,122 @@ namespace SmashCup_AllStars
 
             SpriteSheet spriteSheetP2 = Content.Load<SpriteSheet>("animBlue.sf", new JsonContentLoader());
             _perso2 = new AnimatedSprite(spriteSheetP2);
-
-            // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
             float deltaSeconds = (float)gameTime.ElapsedGameTime.TotalSeconds * 3;
-            float walkSpeed = deltaSeconds * _vitessePerso1;
+            float walkSpeedPerso1 = deltaSeconds * _vitessePerso1;
+            float walkSpeedPerso2 = deltaSeconds * _vitessePerso2;
+            KeyboardState keyboardState = Keyboard.GetState();
+
+
+            //Jump Joueur 1
+            if (jumpingP1)
+            {
+                _perso1Position.Y += jumpspeedP1;//Making it go up
+                jumpspeedP1 += 1;//Some math (explained later)
+                if (_perso1Position.Y >= startYP1)
+                //If it's farther than ground
+                {
+                    _perso1Position.Y = startYP1;//Then set it on
+                    jumpingP1 = false;
+                }
+            }
+            else
+            {
+                if (keyboardState.IsKeyDown(Keys.Z))
+                {
+                    jumpingP1 = true;
+                    jumpspeedP1 = -14;//Give it upward thrust
+                }
+            }
+
+
+            //Jump Joueur 2
+            if (jumpingP2)
+            {
+                _perso2Position.Y += jumpspeedP2;//Making it go up
+                jumpspeedP2 += 1;//Some math (explained later)
+                if (_perso2Position.Y >= startYP2)
+                //If it's farther than ground
+                {
+                    _perso2Position.Y = startYP2;//Then set it on
+                    jumpingP2 = false;
+                }
+            }
+            else
+            {
+                if (keyboardState.IsKeyDown(Keys.Up))
+                {
+                    jumpingP2 = true;
+                    jumpspeedP2 = -14;//Give it upward thrust
+                }
+            }
+
+
+            //Direction dans laquelles regarder
             if (lastDirP1 == "D")
                 animationP1 = "idleD";
             else
                 animationP1 = "idleG";
-
             if (lastDirP2 == "D")
                 animationP2 = "idleD";
             else
                 animationP2 = "idleG";
 
-            KeyboardState keyboardState = Keyboard.GetState();
-            
+            //Deplacement Joueur 1
             if (keyboardState.IsKeyDown(Keys.D))
             {
                 animationP1 = "runD";
-                _perso1Position.X += walkSpeed;
+                _perso1Position.X += walkSpeedPerso1;
                 lastDirP1 = "D";
             }
 
             if (keyboardState.IsKeyDown(Keys.Q))
             {
                 animationP1 = "runG";
-                _perso1Position.X -= walkSpeed;
+                _perso1Position.X -= walkSpeedPerso1;
                 lastDirP1 = "G";
             }
 
 
+            //Deplacement Joueur 2
             if (keyboardState.IsKeyDown(Keys.Right))
             {
                 animationP2 = "runD";
-                _perso2Position.X += walkSpeed;
+                _perso2Position.X += walkSpeedPerso2;
                 lastDirP2 = "D";
             }
 
             if (keyboardState.IsKeyDown(Keys.Left))
             {
                 animationP2 = "runG";
-                _perso2Position.X -= walkSpeed;
+                _perso2Position.X -= walkSpeedPerso2;
                 lastDirP2 = "G";
             }
+
+
             // TODO: Add your update logic here
             _perso1.Play(animationP1);
             _perso2.Play(animationP2);
             _perso1.Update(deltaSeconds);
             _perso2.Update(deltaSeconds);
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Gray);
-            // TODO: Add your drawing code here
+
             _spriteBatch.Begin();
             _spriteBatch.Draw(_perso1, _perso1Position);
             _spriteBatch.Draw(_perso2, _perso2Position);
             _spriteBatch.End();
+
             base.Draw(gameTime);
         }
     }
