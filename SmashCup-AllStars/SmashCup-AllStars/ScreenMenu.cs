d0ac320a8917bb247a8b943a4b0b672c60cf347e
UnGxb <gabi74200.oliveira@gmail.com>
2022-01-11 11:47:01 +0100

    Button trop styler sa mere

--- SmashCup-AllStars/SmashCup-AllStars/ScreenMenu.cs
+++ SmashCup-AllStars/SmashCup-AllStars/ScreenMenu.cs
@@ -21,6 +21,16 @@ namespace SmashCup_AllStars
         private Game1 _game1;
         private Texture2D _backgroundImageMenu;
 
+        private Texture2D _buttonPlay;
+        private Texture2D _buttonOption;
+        private Texture2D _buttonQuit;
+
+        private Vector2 _positionButtonPlay;
+        private Vector2 _positionButtonOption;
+        private Vector2 _positionButtonQuit;
+
+
+
         public static int WIDTH_WINDOW = 1200;
         public static int HEIGHT_WINDOW = 700;
 
@@ -39,12 +49,19 @@ namespace SmashCup_AllStars
             _game1.Graphics.IsFullScreen = false;
             _game1.Graphics.ApplyChanges();
 
+            _positionButtonPlay = new Vector2(950, 525);
+            _positionButtonOption = new Vector2(950, 400);
+            _positionButtonQuit= new Vector2(950, 300);
 
         }
 
         public override void LoadContent()
         {
             _backgroundImageMenu = Content.Load<Texture2D>("bgMenu");
+            _buttonPlay = Content.Load<Texture2D>("play");
+            _buttonOption = Content.Load<Texture2D>("option");
+            _buttonQuit = Content.Load<Texture2D>("quit");
+
 
             base.LoadContent();
         }
@@ -69,6 +86,9 @@ namespace SmashCup_AllStars
 
             _game1.SpriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, transformMatrix: matrix);
             _game1.SpriteBatch.Draw(_backgroundImageMenu, new Vector2(scaleX, scaleY), Color.White);
+            _game1.SpriteBatch.Draw(_buttonPlay, _positionButtonPlay, Color.White);
+            _game1.SpriteBatch.Draw(_buttonOption, _positionButtonOption, Color.White);
+            _game1.SpriteBatch.Draw(_buttonQuit, _positionButtonQuit, Color.White);
             _game1.SpriteBatch.End();
             
 
